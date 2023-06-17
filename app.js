817e99444fcbb616eeb768112a373fd7cc595f50
ChrisGuenther333 <CHRIS.GUENTHER4328@STU.MTEC.EDU>
2023-06-17 10:50:18 -0600

    Added some jQuery to app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$('li:even').css('background-color', '#ccc');
+$('li:odd').css('background-color', '#aaa');
+//Need to change input value to Chris but it's not working
+$('input').text('Chris');
